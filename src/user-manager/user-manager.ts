95e4b3dbb85842f496c4f30fa68810230b4444e4
Pei-Yao Hung <peiyaoh@gmail.com>
2025-07-23 23:00:20 -0400

    Refactor user manager for improved cache and identifier logic
    Simplifies unique identifier checks and user addition by consolidating logic and removing redundant functions. Adds new cache-based retrieval and deletion methods, ensures cache consistency on user operations, and updates the UserManager export to reflect these changes. Removes deprecated or redundant functions for clarity and maintainability.

--- src/user-manager/user-manager.ts
+++ src/user-manager/user-manager.ts
@@ -135,20 +135,18 @@ export const addUser = async (
   user: NewUserRecord
 ): Promise<(JUser | null)> => {
     _checkInitialization();
-    
+
   if (!user || typeof user !== "object" || Array.isArray(user)) {
     const msg = `Invalid user data: ${JSON.stringify(user)}. It must be a non-null object and should not be an array.`;
     Log.warn(msg);
     return null;
   }
 
-  const userUniqueIdentifierExist = await doesUserUniqueIdentifierExist(user);
-  const userDataCheck = await isUserUniqueIdentifierNew(
-    user["uniqueIdentifier"]
-  );
-  if (!userUniqueIdentifierExist["result"] || !userDataCheck["result"]) {
+  const userDataCheck = await isIdentifierUnique(user["uniqueIdentifier"]);
+
+  if (!userDataCheck) {
     Log.warn(
-      `${userDataCheck["message"]} - User: ${JSON.stringify(user)}`
+      `User's unique identifier is not unique: ${JSON.stringify(user)}`
     );
     return null;
   }
@@ -160,6 +158,7 @@ export const addUser = async (
       USERS,
       convertedUser
     )) as JUser;
+    _users.set(addedUser.id, addedUser);
     return addedUser;
   } catch (error) {
     return handleDbError("Failed to add users:", error);
@@ -199,6 +198,29 @@ export const addUsers = async (
   }
 };
 
+
+/**
+ * Retrieves all cached users.
+ *
+ * @returns {JUser[]} An array of all cached users.
+ */
+const getAllUsers = (): JUser[] => {
+  _checkInitialization();
+  return Array.from(_users.values());
+};
+
+
+/**
+ * Retrieves a user by their unique identifier from the cache.
+ * @returns {JUser | null} The user with the specified unique identifier, or null if not found.
+ */
+const getUserByUniqueIdentifier = (uniqueIdentifier: string): JUser | null => {
+  _checkInitialization();
+  return Array.from(_users.values()).find(user => user.uniqueIdentifier === uniqueIdentifier) || null;
+};
+
+
+
 /**
  * Update the properties of a user by uniqueIdentifier
  * @param {string} userUniqueIdentifier - the uniqueIdentifier value.
@@ -229,27 +251,8 @@ const updateUserByUniqueIdentifier = async (
     Log.warn(msg);
     throw new Error(msg);
   }
-
-  const userList: JUser[] | null =
-    await dm.findItemsInCollection<JUser>(
-      USERS,
-      {
-        uniqueIdentifier: userUniqueIdentifier,
-      }
-    );
-
-  if (!userList || userList.length === 0) {
-    const msg = `User with uniqueIdentifier: ${userUniqueIdentifier} not found.`;
-    throw new Error(msg);
-  }
-
-  // throw error if more than one user found with the same uniqueIdentifier
-  if (userList.length > 1) {
-    const msg = `Multiple users found with uniqueIdentifier: ${userUniqueIdentifier}. Please ensure uniqueIdentifier is unique.`;
-    throw new Error(msg);
-  }
-
-  const theUser = userList[0];
+  
+  const theUser: JUser = await getUserByUniqueIdentifier(userUniqueIdentifier) as JUser;
 
   const {
     id,
@@ -290,171 +293,16 @@ const updateUserById = async (
 
 
 
-
-
-
-
 /**
- * Modify the uniqueIdentifier of a user
- * @param {string} id - the user id.
- * @param {string} userUniqueIdentifierValueNew - the new uniqueIdentifier value.
- * @returns {Promise<object | null>} Resolves with the updated item or `null` on error.
- */
-const modifyUserUniqueIdentifier = async (
-  id: string,
-  userUniqueIdentifierValueNew: string
-): Promise<JUser | null> => {
-
-  if (!id || !userUniqueIdentifierValueNew) {
-    const msg = `Invalid parameters: id (${id}) and userUniqueIdentifierValueNew (${userUniqueIdentifierValueNew}) are required.`;
-    Log.warn(msg);
-    throw new Error(msg);
-  }
-
-  const updatedUser: JUser | null =
-    (await dm.updateItemByIdInCollection(USERS, id, {
-      uniqueIdentifier: userUniqueIdentifierValueNew,
-    })) as JUser;
-  return updatedUser;
-};
-
-
-
-/**
- * Confirm that unique identifier is present for a user.
- * @param {object} user - the user object.
- * @returns {boolean, string} An object containing the result (true if uniqueIdentifier exists, false otherwise) and message.
- */
-const doesUserUniqueIdentifierExist = (user: {
-  [key: string]: any;
-}): { result: boolean; message: string } => {
-
-  if (typeof user !== "object" || !user || Array.isArray(user)) {
-    const msg = `Invalid user data: ${JSON.stringify(user)}. It must be a non-null object and should not be an array.`;
-    Log.warn(msg);
-    return { result: false, message: msg };
-  }
-
-  if (!("uniqueIdentifier" in user) || !user["uniqueIdentifier"]) {
-    const msg = `UniqueIdentifier is missing.`;
-    Log.warn(msg);
-    return { result: false, message: msg };
-  }
-  return { result: true, message: "User unique identifier exists." };
-};
-
-/**
- * Check for unique identifier duplication.
- * @param {string} userUniqueIdentifier - the unique identifier.
- * @returns {Promise<{ result: boolean; message: string }>} Resolves with an object containing the result and message.
- * If the unique identifier is new, it returns true; otherwise, it returns false.
- */
-
-const isUserUniqueIdentifierNew = async (
-  userUniqueIdentifier: string
-): Promise<{ result: boolean; message: string }> => {
-  if (
-    !userUniqueIdentifier ||
-    typeof userUniqueIdentifier !== "string" ||
-    userUniqueIdentifier.trim() === ""
-  ) {
-    const msg = `Invalid unique identifier: ${userUniqueIdentifier}`;
-    Log.warn(msg);
-    return { result: false, message: msg };
-  }
-
-  const existingUsers =
-    await dm.findItemsInCollection<JUser>(
-      USERS,
-      {
-        uniqueIdentifier: userUniqueIdentifier,
-      }
-    );
-
-  if (existingUsers && existingUsers.length > 0) {
-    const msg = `User with unique identifier (${userUniqueIdentifier}) already exists.`;
-    Log.warn(msg);
-    return { result: false, message: msg };
-  }
-  return { result: true, message: `User's unique identifier is valid.` };
-};
-
-/**
- * Adds multiple users to the Users collection in a single operation.
- * @param {object[]} users - An array of user objects to add.
- * @returns {Promise<(JUser | null)[]>} Resolves with the added users or null if the operation fails.
- * @throws {Error} If no users are provided or if any user fails validation.
- */
-export const addUsersToDatabase = async (
-  users: object[]
-): Promise<(JUser | null)[]> => {
-  if (!Array.isArray(users) || users.length === 0) {
-    throw new Error("No users provided for insertion.");
-  }
-
-  try {
-    let newUsers: object[] = [];
-
-    for (const user of users) {
-      if (typeof user !== "object" || !user || Array.isArray(user)) {
-        const msg = `Invalid user data: ${JSON.stringify(user)}. It must be a non-null object and should not be an array.`;
-        Log.warn(msg);
-        continue; // Skip invalid user
-      }
-      const userUniqueIdentifierExist =
-        await doesUserUniqueIdentifierExist(user);
-      const userWithId = user as {
-        uniqueIdentifier: string;
-        [key: string]: any;
-      };
-      const userDataCheck = await isUserUniqueIdentifierNew(
-        userWithId["uniqueIdentifier"]
-      );
-      // print the two checks
-      Log.dev(`User unique identifier check: ${JSON.stringify(userUniqueIdentifierExist)}`);
-      Log.dev(`User data check: ${JSON.stringify(userDataCheck)}`);
-      if (!userUniqueIdentifierExist["result"] || !userDataCheck["result"]) {
-        Log.warn(
-          `${userDataCheck["message"]} - User: ${JSON.stringify(userWithId)}`
-        );
-      } else {
-        newUsers.push(user);
-      }
-    }
-
-    let addedUsers: (JUser | null)[] = [];
-    for (const user of newUsers) {
-      const addedUser = (await dm.addItemToCollection(
-        USERS,
-        user
-      )) as JUser;
-      addedUsers.push(addedUser);
-    }
-    return addedUsers;
-  } catch (error) {
-    return handleDbError("Failed to add users:", error);
-  }
-};
-
-/**
- * Creates a new user with optional initial data.
- * Saves to both the database and the in-memory cache.
+ * Deletes a user by ID from both the database and the in-memory cache.
  *
- * @param {object} initialData - Initial user data.
- * @returns {Promise<JUser>} Resolves to the created user.
+ * @param {string} userId - The user's ID.
+ * @returns {Promise<void>} Resolves when deletion is complete.
  */
-const createUser = async (initialData: object = {}): Promise<JUser> => {
-  _checkInitialization();
-  const addedUser = (await dm.addItemToCollection(
-    USERS,
-    initialData
-  )) as JUser;
-  if (!addedUser) {
-    throw new Error("Failed to create user: result is null");
-  }
-  _users.set(addedUser.id, addedUser);
-  Log.info('User created:', addedUser);
-  return addedUser;
+const deleteUserByUniqueIdentifier = async (uniqueIdentifier: string): Promise<void> => {
+  const theUser: JUser = await getUserByUniqueIdentifier(uniqueIdentifier) as JUser;
+  await deleteUserById(theUser.id);
+  _users.delete(theUser.id);
 };
 
 /**
@@ -463,36 +311,11 @@ const createUser = async (initialData: object = {}): Promise<JUser> => {
  * @param {string} userId - The user's ID.
  * @returns {Promise<void>} Resolves when deletion is complete.
  */
-const deleteUser = async (userId: string): Promise<void> => {
+const deleteUserById = async (userId: string): Promise<void> => {
   _checkInitialization();
   await dm.removeItemFromCollection(USERS, userId);
   _users.delete(userId);
 };
-
-
-
-/**
- * Retrieves all cached users.
- *
- * @returns {JUser[]} An array of all cached users.
- */
-const getAllUsers = (): JUser[] => {
-  _checkInitialization();
-  return Array.from(_users.values());
-};
-
-/**
- * Retrieves a user from the cache by ID.
- *
- * @param {string} userId - The user's ID.
- * @returns {JUser | null} The user if found, otherwise `null`.
- */
-const getUser = (userId: string): JUser | null => {
-  _checkInitialization();
-  return _users.get(userId) || null;
-};
-
-
 
 /**
  * Deletes all users from the database and clears the in-memory cache.
@@ -505,6 +328,37 @@ const deleteAllUsers = async (): Promise<void> => {
   _users.clear();
 };
 
+/**
+ * Check for unique identifier duplication.
+ * @param {string} userUniqueIdentifier - the unique identifier.
+ * @returns {Promise<boolean>} Resolves with a boolean indicating if the identifier is unique.
+ * If the unique identifier is new, it returns true; otherwise, it returns false.
+ */
+
+const isIdentifierUnique = async (
+  userUniqueIdentifier: string
+): Promise<boolean> => {
+
+  if (
+    !userUniqueIdentifier ||
+    typeof userUniqueIdentifier !== "string" ||
+    userUniqueIdentifier.trim() === ""
+  ) {
+    const msg = `Invalid unique identifier: ${userUniqueIdentifier}`;
+    throw new Error(msg);
+  }
+
+  const existingUser:JUser | null = await getUserByUniqueIdentifier(userUniqueIdentifier) as JUser;
+
+  if (existingUser) {
+    const msg = `User with unique identifier (${userUniqueIdentifier}) already exists.`;
+    Log.warn(msg);
+    return false;
+  }
+
+  return true;
+};
+
 
 
 /**
@@ -515,16 +369,13 @@ const deleteAllUsers = async (): Promise<void> => {
  */
 export const UserManager = {
   init,
-  createUser,
   addUsersToDatabase: addUsers,
-  deleteUser,
-  modifyUserUniqueIdentifier,
+  deleteUser: deleteUserById,
   updateUserByUniqueIdentifier,
-  doesUserUniqueIdentifierExist,
-  isUserUniqueIdentifierNew,
   getAllUsers,
-  getUser,
+  getUserByUniqueIdentifier,
   updateUser: updateUserById,
+  deleteUserByUniqueIdentifier,
   deleteAllUsers,
   shutdown: shutdown,
 };
@@ -539,5 +390,7 @@ export const TestingUserManager = {
   ...UserManager,
   _checkInitialization,
   refreshCache,
+  deleteUserById,
+  isIdentifierUnique,
   _users, // Exposes the in-memory cache for testing purposes
 };
