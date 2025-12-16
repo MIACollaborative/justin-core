23dbbcea1b2e3ecc62ed702fc16bcded56eb4a87
Pei-Yao Hung <peiyaoh@gmail.com>
2025-12-16 12:55:29 -0500

    Integrate ProtectedManager into UserManager
    Added initialization of ProtectedManager in the UserManager's init function and exposed get, set, and delete methods for protected attributes through UserManager. This enables management of protected user attributes via the UserManager interface.

--- src/user-manager/user-manager.ts
+++ src/user-manager/user-manager.ts
@@ -5,6 +5,8 @@ import { JUser, NewUserRecord } from './user.type';
 import { handleDbError } from '../data-manager/data-manager.helpers';
 import { CollectionChangeType } from '../data-manager/data-manager.type';
 import { createLogger } from '../logger/logger';
+import { ProtectedManager } from '../protected-manager/protected-manager';
+import { set } from 'zod';
 
 const Log = createLogger({
   context: {
@@ -39,6 +41,9 @@ const init = async (): Promise<void> => {
 
   await refreshCache();
   setupChangeListeners();
+
+  // protected-manager init call
+  await ProtectedManager.init();
 };
 
 /**
@@ -403,6 +408,9 @@ export const UserManager = {
   deleteUserByUniqueIdentifier,
   deleteAllUsers,
   shutdown,
+  getProtectedAttributes: ProtectedManager.getProtectedAttributes,
+  setProtectedAttributes: ProtectedManager.setProtectedAttributes,
+  deleteProtectedAttributes: ProtectedManager.deleteProtectedAttributes,
 };
 
 /**
